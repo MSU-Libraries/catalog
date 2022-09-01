601c772b0fedcaad029f66a97f0b61b0d24539d2
Nathan Collins <colli372@msu.edu>
2022-09-01 14:16:57 -0400

    cron: alpha-browse build can now use temp build dir

--- solr/alpha-browse.sh
+++ solr/alpha-browse.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+DEFAULT_BUILD_PATH=/tmp/alpha-browse/build
 DEFAULT_SHARED_PATH=/mnt/shared/alpha-browse
 DEFAULT_MAX_AGE_HOURS=6
 
@@ -20,6 +21,8 @@ runhelp() {
     echo "     Will rebuild the database files only if they are more than 12 hours old."
     echo ""
     echo "FLAGS:"
+    echo "  -b|--build-path PATH"
+    echo "     Working directory to build new databases in. Default: ${DEFAULT_BUILD_PATH}"
     echo "  -p|--shared-path PATH"
     echo "     Path to the already cloned VuFind repository. Default: ${DEFAULT_SHARED_PATH}"
     echo "  -a|--max-age-hours"
@@ -41,6 +44,7 @@ fi
 # Set defaults
 default_args() {
     declare -g -A ARGS
+    ARGS[BUILD_PATH]="${DEFAULT_BUILD_PATH}"
     ARGS[SHARED_PATH]="${DEFAULT_SHARED_PATH}"
     ARGS[MAX_AGE_HOURS]="${DEFAULT_MAX_AGE_HOURS}"
     ARGS[FORCE]=0
@@ -59,6 +63,14 @@ parse_args() {
             ARGS[MAX_AGE_HOURS]="$2"
             if [[ ! "${ARGS[MAX_AGE_HOURS]}" -gt 0 ]]; then
                 echo "ERROR: -a|--max-age-hours only accept positive integers"
+                exit 1
+            fi
+            shift; shift ;;
+        -b|--build-path)
+            ARGS[BUILD_PATH]=$( readlink -f "$2" )
+            RC=$?
+            if [[ "$RC" -ne 0 ]]; then
+                echo "ERROR: -b|--build-path path is not valid: $2"
                 exit 1
             fi
             shift; shift ;;
@@ -95,12 +107,24 @@ rebuild_databases() {
     verbose "Running database rebuild script..."
     RCODE=0
 
-    # Create required symlink if it doesn't already exist
-    if [[ ! -h /bitnami/solr/server/vendor ]]; then
-        ln -s /opt/bitnami/solr /bitnami/solr/server/vendor
+    # Prepare build directory
+    mkdir -p "${ARGS[BUILD_PATH]}/alphabetical_browse"
+
+    # Create required symlinks if they don't already exist
+    if [[ ! -h ${ARGS[BUILD_PATH]}/../vendor ]]; then
+        ln -s /opt/bitnami/solr ${ARGS[BUILD_PATH]}/../vendor
+    fi
+    if [[ ! -h ${ARGS[BUILD_PATH]}/jars ]]; then
+        ln -s /bitnami/solr/server/solr/jars ${ARGS[BUILD_PATH]}/jars
+    fi
+    if [[ ! -h ${ARGS[BUILD_PATH]}/biblio ]]; then
+        ln -s /bitnami/solr/server/solr/biblio ${ARGS[BUILD_PATH]}/biblio
+    fi
+    if [[ ! -h ${ARGS[BUILD_PATH]}/authority ]]; then
+        ln -s /bitnami/solr/server/solr/authority ${ARGS[BUILD_PATH]}/authority
     fi
 
-    if ! JAVA_HOME=/opt/bitnami/java SOLR_HOME=/bitnami/solr/server/solr /solr_confs/index-alphabetic-browse.sh; then
+    if ! JAVA_HOME=/opt/bitnami/java SOLR_HOME=${ARGS[BUILD_PATH]} /solr_confs/index-alphabetic-browse.sh; then
         verbose "Error occured while running index-alphabetic-browse.sh script!"
         RCODE=1
     else
@@ -116,8 +140,8 @@ update_shared() {
     find ${ARGS[SHARED_PATH]} -type f -mmin +$(( ARGS[MAX_AGE_HOURS] * 60 )) -name "*.db*" ! -name "*lock" -delete
 
     # Copy all database files to the shared storage
-    verbose "Copying database files from: /bitnami/solr/server/solr/alphabetical_browse/*db-* to ${ARGS[SHARED_PATH]}"
-    cp -p /bitnami/solr/server/solr/alphabetical_browse/*db-* ${ARGS[SHARED_PATH]}/
+    verbose "Copying database files from: ${ARGS[BUILD_PATH]}/alphabetical_browse/*db-* to ${ARGS[SHARED_PATH]}"
+    cp -p ${ARGS[BUILD_PATH]}/alphabetical_browse/*db-* ${ARGS[SHARED_PATH]}/
 }
 
 lock_state() {
