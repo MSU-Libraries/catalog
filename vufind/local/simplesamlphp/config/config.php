126493d4829b7c634ac6d50f345fd65204744337
Damien <damien-git@users.noreply.github.com>
2022-10-07 14:44:49 -0400

    SAML baseurlpath

--- vufind/local/simplesamlphp/config/config.php
+++ vufind/local/simplesamlphp/config/config.php
@@ -27,7 +27,7 @@ $config = [
      * external url, no matter where you come from (direct access or via the
      * reverse proxy).
      */
-    'baseurlpath' => 'simplesaml/',
+    'baseurlpath' => 'https://'.getenv('SITE_HOSTNAME').'/simplesamlphp/',
 
     /*
      * The 'application' configuration array groups a set configuration options
