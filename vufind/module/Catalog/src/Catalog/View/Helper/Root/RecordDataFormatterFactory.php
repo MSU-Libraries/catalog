f3d2285a2bab27a0dbfc2c629efaef8904203ebc
Megan Schanz <schanzme@msu.edu>
2022-10-10 11:19:55 -0400

    Add genre to record page

---
+++ vufind/module/Catalog/src/Catalog/View/Helper/Root/RecordDataFormatterFactory.php
@@ -0,0 +1,15 @@
+<?php>
+
+namespace Catalog\View\Helper\Root;
+
+use Vufind\View\Helper\Root\RecordDataFormatter\SpecBuilder;
+
+class RecordDataFormatterFactory extends \Vufind\View\Helper\Root\RecordDataFormatterFactory
+{
+    public function getDefaultCoreSpecs()
+    {
+        $spec = new SpecBuilder(parent::getDefaultCoreSpecs());
+        $spect->setLine('Genre', 'getGenre');
+        return $spec->getArray();
+    }
+}
