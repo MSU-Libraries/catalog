5ba2ea2af4e9764ff4456217ae61a79457895685
Nathan Collins <colli372@msu.edu>
2022-11-11 00:08:40 -0500

    vufind: Fix other writable permissions

--- vufind/startup-cachecron.sh
+++ vufind/startup-cachecron.sh
@@ -3,6 +3,7 @@
 # Replace regular cron.d with cachecron.d
 mv /etc/cron.d/ /etc/defaultcron.d/
 mv /etc/cachecron.d/ /etc/cron.d/
+chmod -R g-w,o-w /etc/cron.d/
 
 # Start up syslog (required for cron)
 rsyslogd
