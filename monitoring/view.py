9e7cb94476a33cba71d6c3a01e666346858be814
Damien <damien-git@users.noreply.github.com>
2022-11-21 15:49:43 -0500

    trying to fix traefik routes

--- monitoring/view.py
+++ monitoring/view.py
@@ -1,25 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, send_file
 from pathlib import Path
 import os
+import requests
 
 app = Flask(__name__)
 
+def raise_exception_for_reply(r):
+    raise Exception('Status code: {}. Response: "{}"'.format(r.status_code, r.text))
 
 @app.route('/monitoring/node/logs/vufind')
 def node_logs_vufind():
-    return Path('/mnt/logs/vufind/vufind.log').read_text()
+    return send_file('/mnt/logs/vufind/vufind.log')
 
 @app.route('/monitoring/node/logs/apache/error')
 def node_logs_apache_error():
-    return Path('/mnt/logs/apache/error.log').read_text()
+    return send_file('/mnt/logs/apache/error.log')
 
 @app.route('/monitoring/node/logs/apache/access')
 def node_logs_apache_access():
-    return Path('/mnt/logs/apache/access.log').read_text()
+    return send_file('/mnt/logs/apache/access.log')
 
 @app.route('/monitoring/node/logs/simplesamlphp')
 def node_logs_simplesamlphp():
-    return Path('/mnt/logs/simplesamlphp/simplesamlphp.log').read_text()
+    return send_file('/mnt/logs/simplesamlphp/simplesamlphp.log')
+
+@app.route('/monitoring/logs/<path:service>')
+def logs_vufind(service):
+    logs = ''
+    for node in range(1, 3):
+        logs += '------ NODE ' + node + ' ------\n'
+        r = requests.get('http://monitoring' + node + '/monitoring/node/logs/%s' % service)
+        if r.status_code != 201:
+            raise_exception_for_reply(r)
+        logs += r.text + '\n'
+    return Response(logs, mimetype='text/plain')
 
 @app.route('/monitoring')
 def home():
