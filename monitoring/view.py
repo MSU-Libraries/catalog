75bb61b3775b36d507a9944b799cbab8aee70470
Damien <damien-git@users.noreply.github.com>
2022-12-02 12:48:38 -0500

    monitoring: available memory and disk space

--- monitoring/view.py
+++ monitoring/view.py
@@ -18,16 +18,26 @@ def logs_vufind(service):
 def cluster_state_uuid():
     return status.cluster_state_uuid()
 
+@app.route('/monitoring/node/available_memory')
+def node_available_memory():
+    return status.node_available_memory()
+
+@app.route('/monitoring/node/available_disk_space')
+def node_available_disk_space():
+    return status.node_available_disk_space()
+
 @app.route('/monitoring')
 def home():
     status_list = {}
+    status_list['memory'] = status.get_memory_status()
+    status_list['disk_space'] = status.get_disk_space_status()
     status_list['traefik'] = status.get_traefik_status()
     status_list['galera'] = status.get_galera_status()
     status_list['solr'] = status.get_solr_status()
     status_list['vufind'] = status.get_vufind_status()
     services = {}
     for s_name, s_text in status_list.items():
-        if s_text == 'OK':
+        if s_text.startswith('OK'):
             color = 'success'
         else:
             color = 'danger'
