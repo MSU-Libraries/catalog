e0d02f74854c0d30091bb69a0c129b49d8226f7c
Damien <damien-git@users.noreply.github.com>
2022-12-06 12:03:07 -0500

    monitoring: graphs

--- monitoring/static/js/plot.js
+++ monitoring/static/js/plot.js
@@ -6,7 +6,7 @@ function plot(node, pt_x, pt_y, data_name, period) {
     };
     var data = [ trace1 ];
     var layout = { 
-        title: data_name + ' for the last ' + period,
+        title: data_name.replaceAll('_', ' ') + ' for the last ' + period,
         font: { size: 16 },
         yaxis: {
             title: 'Minimum ' + data_name.replaceAll('_', ' ') + ' (%)'
