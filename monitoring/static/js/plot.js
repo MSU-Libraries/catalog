37d324e8ce0c38e8ded52d14b8e2d3f93ca023be
Damien <damien-git@users.noreply.github.com>
2022-12-08 12:18:29 -0500

    Monitoring: fixes

--- monitoring/static/js/plot.js
+++ monitoring/static/js/plot.js
@@ -1,6 +1,6 @@
 function plot(variable, period, data) {
     var variable_with_spaces = variable.replaceAll('_', ' ')
-    var ytitle = variable == 'apache_requests' ? 'Unfiltered apache requests' :
+    var ytitle = variable == 'apache_requests' ? 'Apache hits / minute' :
         'Minimum ' + variable_with_spaces + ' (%)';
     var title = variable_with_spaces.charAt(0).toUpperCase() + variable_with_spaces.slice(1) +
         ' for the last ' + period;
