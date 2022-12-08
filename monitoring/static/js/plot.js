e66fd0bfccb1a5ada54b2a97ce4468feb17e4d36
Damien <damien-git@users.noreply.github.com>
2022-12-08 11:41:06 -0500

    [PC-280] Monitoring: merge node plots

--- monitoring/static/js/plot.js
+++ monitoring/static/js/plot.js
@@ -1,19 +1,16 @@
-function plot(node, pt_x, pt_y, data_name, period) {
-    var trace1 = {
-        type: 'scatter',
-        x: pt_x,
-        y: pt_y
-    };
-    var data = [ trace1 ];
-    var ytitle = data_name == 'apache_requests' ? 'unfiltered apache requests' :
-        'Minimum ' + data_name.replaceAll('_', ' ') + ' (%)';
+function plot(variable, period, data) {
+    var variable_with_spaces = variable.replaceAll('_', ' ')
+    var ytitle = data_name == 'apache_requests' ? 'Unfiltered apache requests' :
+        'Minimum ' + variable_with_spaces + ' (%)';
+    var title = variable_with_spaces.charAt(0).toUpperCase() + variable_with_spaces.slice(1) +
+        ' for the last ' + period;
     var layout = { 
-        title: data_name.replaceAll('_', ' ') + ' for the last ' + period,
+        title: title,
         font: { size: 16 },
         yaxis: {
             title: ytitle
         }
     };
     var config = { responsive: true };
-    Plotly.newPlot('graph'+node, data, layout, config);
+    Plotly.newPlot('plot', data, layout, config);
 }
