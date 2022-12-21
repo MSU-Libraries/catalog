7d9d6282fb612cb739cdb3349c41d434b7c2d982
Nathan Collins <colli372@msu.edu>
2022-12-21 13:43:24 -0500

    saml: Remove test env; update qa env vars (it now uses Okta prod)

--- vufind/simplesamlphp/local/metadata/saml20-idp-remote.php
+++ vufind/simplesamlphp/local/metadata/saml20-idp-remote.php
@@ -35,19 +35,11 @@ $metadata['http://www.okta.com/exk1ed76cimmz56WW0h8'] = [
     'certificate'          => 'msu-dev.pem',
     'sign.logout'          => true,
 ];
-// MSU TEST (can be used with catalog-beta)
-$metadata['http://www.okta.com/exk1eh7tv3kMdzJgn0h8'] = [
-    'name'                 => 'MSU TEST IdP',
-    'SingleSignOnService'  => 'https://auth.test.itservices.msu.edu/app/msutst_testlibpubliccatalog_1/exk1eh7tv3kMdzJgn0h8/sso/saml',
-    'SingleLogoutService'  => 'https://auth.test.itservices.msu.edu/login/signout?fromURI=https://catalog-beta.lib.msu.edu',
-    'certificate'          => 'msu-test.pem',
-    'sign.logout'          => true,
-];
-// MSU QA (can be used with catalog-beta)
-$metadata['http://www.okta.com/exk1eh7u7cb8BIF9z0h8'] = [
+// MSU QA (can be used with catalog-beta); running on Okta prod to avoid IP restrictions
+$metadata['http://www.okta.com/exko9ngr82UjBdFPU357'] = [
     'name'                 => 'MSU QA IdP',
-    'SingleSignOnService'  => 'https://auth.qual.itservices.msu.edu/app/msuqa_qalibpubliccatalog_1/exk1eh7u7cb8BIF9z0h8/sso/saml',
-    'SingleLogoutService'  => 'https://auth.qual.itservices.msu.edu/login/signout?fromURI=https://catalog-beta.lib.msu.edu',
+    'SingleSignOnService'  => 'https://auth.msu.edu/app/msu_qalibpubliccatalog_1/exko9ngr82UjBdFPU357/sso/saml',
+    'SingleLogoutService'  => 'https://auth.msu.edu/login/signout?fromURI=https://catalog-beta.lib.msu.edu',
     'certificate'          => 'msu-qa.pem',
     'sign.logout'          => true,
 ];
