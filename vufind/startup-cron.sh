341c0b6d793ac39011a98cb2d559473888c631db
Nathan Collins <colli372@msu.edu>
2023-01-05 13:04:14 -0500

    harvest: Correct OAI harvest directory structure

--- vufind/startup-cron.sh
+++ vufind/startup-cron.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Map folio harvest directory onto the shared storage (need the added storage capacity)
-mkdir -p /mnt/shared/oai/${STACK_NAME}_harvest_folio/ /mnt/shared/oai/${STACK_NAME}_current/
+mkdir -p /mnt/shared/oai/${STACK_NAME}/harvest_folio/ /mnt/shared/oai/${STACK_NAME}/current/
 mv /usr/local/vufind/local/harvest/folio/ /tmp/
-ln -s /mnt/shared/oai/${STACK_NAME}_harvest_folio/ /usr/local/vufind/local/harvest/folio
-ln -s /mnt/shared/oai/${STACK_NAME}_current/ /mnt/oai_current
+ln -s /mnt/shared/oai/${STACK_NAME}/harvest_folio/ /usr/local/vufind/local/harvest/folio
+ln -s /mnt/shared/oai/${STACK_NAME}/ /mnt/oai
 
 # Save the logs in the logs docker volume
 mkdir -p /mnt/logs/vufind /mnt/logs/harvests
