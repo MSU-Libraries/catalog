069c74578bbbe457565e59abf33a0ddd188f4425
Damien Guillaume <damieng@msu.edu>
2023-08-29 14:11:53 -0400

    Changed latest log file names to make it easier for monitoring to find related latest log

--- vufind/scripts/cron-sessions.sh
+++ vufind/scripts/cron-sessions.sh
@@ -3,7 +3,7 @@
 # Cron job - Clear out old sessions from the database
 
 export CRON_COMMAND="/usr/bin/php /usr/local/vufind/public/index.php util/expire_sessions"
-export LATEST_PATH=/mnt/logs/vufind/sessions_latest.log
+export LATEST_PATH=/mnt/logs/vufind/sessions_cleanup_latest.log
 export LOG_PATH=/mnt/logs/vufind/sessions_cleanup.log
 export EXIT_CODE_PATH=/mnt/logs/vufind/sessions_exit_code
 export DOCKER_TAG=EXPIRE_SESSIONS
