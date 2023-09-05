37ee7bcfe391219411fc88b4d2e6ef2f2a8b21b8
Damien Guillaume <damieng@msu.edu>
2023-09-05 16:08:11 -0400

    test

---
+++ vufind/module/Catalog/src/Catalog/RecordDriver/EDS.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Extension of VuFind model for EDS records.
+ *
+ * PHP version 8
+ *
+ * @category VuFind
+ * @package  RecordDrivers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:record_drivers Wiki
+ */
+
+namespace Catalog\RecordDriver;
+
+/**
+ * Model for EDS records.
+ *
+ * @category VuFind
+ * @package  RecordDrivers
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:record_drivers Wiki
+ */
+class EDS extends \VuFind\RecordDriver\EDS
+{
+    /**
+     * Return a URL to a thumbnail preview of the record, if available; false
+     * otherwise.
+     *
+     * @param string $size Size of thumbnail (small, medium or large -- small is
+     * default).
+     *
+     * @return string
+     */
+    public function getThumbnail($size = 'small')
+    {
+        foreach ($this->fields['ImageInfo'] ?? [] as $image) {
+            if ($size == ($image['Size'] ?? '')) {
+                return $image['Target'] ?? '';
+            }
+        }
+        if (!empty($this->fields['Items'] ?? []) || empty($this->getAccessLevel())) {
+            return parent::getThumbnail($size);
+        } else {
+            return false;
+        }
+    }
+}
