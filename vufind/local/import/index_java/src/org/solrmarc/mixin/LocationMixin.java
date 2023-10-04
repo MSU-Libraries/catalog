e8e9c272e93609ae4ca665c8cd60f488e876d8b6
Damien Guillaume <damieng@msu.edu>
2023-10-04 14:49:00 -0400

    [PC-741] Avoid indexing location of suppressed holdings

--- vufind/local/import/index_java/src/org/solrmarc/mixin/LocationMixin.java
+++ vufind/local/import/index_java/src/org/solrmarc/mixin/LocationMixin.java
@@ -20,40 +20,47 @@ public class LocationMixin extends SolrIndexerMixin {
             return result;
         }
         List<VariableField> locationFields = record.getVariableFields("952");
-        List<Subfield> cSubfields = getSubfieldsMatching(locationFields, "c");
-        List<Subfield> dSubfields = getSubfieldsMatching(locationFields, "d");
-        List<String> cValues = cSubfields.stream().map(f -> f.getData().replace("/", " ").trim()).collect(Collectors.toList());
-        List<String> dValues = dSubfields.stream().map(f -> f.getData().replace("/", " ").trim()).collect(Collectors.toList());
-        cValues.forEach(c -> {
-            result.add("0/" + c + "/");
-        });
-        dValues.forEach(d -> {
-            if (d.contains("-")) {
-                int index = d.indexOf("-");
-                String first = d.substring(0, index).trim();
-                String second = d.substring(index + 1).trim();
-                if (cValues.contains(first))
-                    result.add("1/" + first + "/" + second + "/");
+        locationFields.forEach(location -> {
+            List<Subfield> tSubfields = getSubfieldsMatching(location, "t");
+            if (tSubfields.stream().anyMatch(t -> "1".equals(t.getData().trim()))) {
+                // Ignore location when 952t is 1
+                continue;
+            }
+            List<Subfield> cSubfields = getSubfieldsMatching(location, "c");
+            List<Subfield> dSubfields = getSubfieldsMatching(location, "d");
+            List<String> cValues = cSubfields.stream().map(f -> f.getData().replace("/", " ").trim()).collect(Collectors.toList());
+            List<String> dValues = dSubfields.stream().map(f -> f.getData().replace("/", " ").trim()).collect(Collectors.toList());
+            cValues.forEach(c -> {
+                result.add("0/" + c + "/");
+            });
+            dValues.forEach(d -> {
+                if (d.contains("-")) {
+                    int index = d.indexOf("-");
+                    String first = d.substring(0, index).trim();
+                    String second = d.substring(index + 1).trim();
+                    if (cValues.contains(first)) {
+                        result.add("1/" + first + "/" + second + "/");
+                    }
+                }
+            });
+            for (int i = 0; i < cValues.size() && i < dValues.size(); i++) {
+                String c = cValues.get(i);
+                String d = dValues.get(i);
+                if (c != null && d != null && !c.equals(d) && !d.contains("-")) {
+                    result.add("1/" + c + "/" + d + "/");
+                }
             }
         });
-        for (int i = 0; i < cValues.size() && i < dValues.size(); i++) {
-            String c = cValues.get(i);
-            String d = dValues.get(i);
-            if (c != null && d != null && !c.equals(d) && !d.contains("-")) {
-                result.add("1/" + c + "/" + d + "/");
-            }
-        }
-        return result;
+        return result.stream().distinct().collect(Collectors.toList());
     }
 
-    private List<Subfield> getSubfieldsMatching(List<VariableField> fields, String subfieldCodes) {
+    private List<Subfield> getSubfieldsMatching(VariableField vf, String subfieldCodes) {
         ArrayList<Subfield> subfields = new ArrayList<Subfield>();
-        for (VariableField vf : fields) {
-            if (!(vf instanceof DataField))
-                return(subfields);
-            DataField df = (DataField)vf;
-            subfields.addAll(df.getSubfields(subfieldCodes));
+        if (!(vf instanceof DataField)) {
+            return(subfields);
         }
+        DataField df = (DataField)vf;
+        subfields.addAll(df.getSubfields(subfieldCodes));
         return(subfields);
     }
 
