43f246253f2484a54872a3934c79441b60435869
Damien Guillaume <damieng@msu.edu>
2023-10-23 11:16:57 -0400

    [PC-820] Avoid storing secrets in Docker images

--- vufind/entrypoint.sh
+++ vufind/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Entrypoint script..."
+
 # Replace environment variables in template ini files
 envsubst < local/config/vufind/config.ini | sponge local/config/vufind/config.ini
 envsubst < local/config/vufind/contentsecuritypolicy.ini | sponge local/config/vufind/contentsecuritypolicy.ini
@@ -10,23 +12,35 @@ envsubst < local/harvest/oai.ini | sponge local/harvest/oai.ini
 envsubst < /etc/aliases | sponge /etc/aliases
 
 # Set required environment variables so cron jobs have access to them
-echo JAVA_HOME="$JAVA_HOME" >> /etc/environment
-echo VUFIND_HOME="$VUFIND_HOME"  >> /etc/environment
-echo VUFIND_LOCAL_DIR="$VUFIND_LOCAL_DIR" >> /etc/environment
-echo VUFIND_CACHE_DIR="$VUFIND_CACHE_DIR" >> /etc/environment
-echo VUFIND_LOCAL_MODULES="Catalog" >> /etc/environment
-echo FTP_USER="$FTP_USER" >> /etc/environment
-echo FTP_PASSWORD="$FTP_PASSWORD" >> /etc/environment
-echo AUTH_FTP_USER="$AUTH_FTP_USER" >> /etc/environment
-echo AUTH_FTP_PASSWORD="$AUTH_FTP_PASSWORD" >> /etc/environment
-echo DEPLOY_KEY="$DEPLOY_KEY" >> /etc/environment
-echo BROWZINE_LIBRARY="$BROWZINE_LIBRARY" >> /etc/environment
-echo BROWZINE_TOKEN="$BROWZINE_TOKEN" >> /etc/environment
-echo STACK_NAME="$STACK_NAME" >> /etc/environment
+if ! grep -q STACK_NAME /etc/environment; then
+    echo JAVA_HOME="$JAVA_HOME" >> /etc/environment
+    echo VUFIND_HOME="$VUFIND_HOME"  >> /etc/environment
+    echo VUFIND_LOCAL_DIR="$VUFIND_LOCAL_DIR" >> /etc/environment
+    echo VUFIND_CACHE_DIR="$VUFIND_CACHE_DIR" >> /etc/environment
+    echo VUFIND_LOCAL_MODULES="Catalog" >> /etc/environment
+    echo FTP_USER="$FTP_USER" >> /etc/environment
+    echo FTP_PASSWORD="$FTP_PASSWORD" >> /etc/environment
+    echo AUTH_FTP_USER="$AUTH_FTP_USER" >> /etc/environment
+    echo AUTH_FTP_PASSWORD="$AUTH_FTP_PASSWORD" >> /etc/environment
+    echo DEPLOY_KEY="$DEPLOY_KEY" >> /etc/environment
+    echo BROWZINE_LIBRARY="$BROWZINE_LIBRARY" >> /etc/environment
+    echo BROWZINE_TOKEN="$BROWZINE_TOKEN" >> /etc/environment
+    echo STACK_NAME="$STACK_NAME" >> /etc/environment
+fi
+
+# Finish SimpleSAMLphp config setup
+envsubst '${SIMPLESAMLPHP_SALT} ${SIMPLESAMLPHP_ADMIN_PW}' < ${SIMPLESAMLPHP_CONFIG_DIR}/config.php | \
+    sponge ${SIMPLESAMLPHP_CONFIG_DIR}/config.php
 
 # Unset env variables that are just used in config files and don't need to be in the environment after this
 unset FOLIO_URL FOLIO_USER FOLIO_PASS FOLIO_TENANT FOLIO_REC_ID FOLIO_CANCEL_ID OAI_URL MAIL_HOST MAIL_PORT \
     MAIL_USERNAME MAIL_PASSWORD FEEDBACK_EMAIL EDS_USER EDS_PASS EDS_PROFILE EDS_ORG RECAPTCHA_SITE_KEY \
-    RECAPTCHA_SECRET_KEY MATOMO_URL MATOMO_SITE_ID MATOMO_SEARCHBACKEND_DIMENSION
+    RECAPTCHA_SECRET_KEY MATOMO_URL MATOMO_SITE_ID MATOMO_SEARCHBACKEND_DIMENSION SIMPLESAMLPHP_SALT SIMPLESAMLPHP_ADMIN_PW
+
+echo "Startup script..."
+
+echo "Startup arguments: $@"
+echo "env: "
+env
 
 exec "$@"
