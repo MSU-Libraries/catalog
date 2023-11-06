e6f5e7fef668bff8fb5d5b022079ea33576e2d3a
Damien Guillaume <damieng@msu.edu>
2023-11-06 10:12:20 -0500

    Monitoring: added Solr logs

--- solr/startup.sh
+++ solr/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Save all the Solr logs in the logs docker volume
-mkdir -p /mnt/logs/solr
 if [[ ! -L /opt/bitnami/solr/logs ]]; then
     rm -rf /opt/bitnami/solr/logs
     ln -s /mnt/logs/solr /opt/bitnami/solr/logs
