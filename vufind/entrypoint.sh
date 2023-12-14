fba6bc5972a2a06c607215c71b377730ed0bf882
Damien Guillaume <damieng@msu.edu>
2023-12-14 12:37:49 -0500

    [PC-634] Simplify code after upgrade to VuFind 9.1

--- vufind/entrypoint.sh
+++ vufind/entrypoint.sh
@@ -34,7 +34,9 @@ if [[ "$1" == "/startup-cron.sh" ]]; then
         echo JAVA_HOME="$JAVA_HOME" >> /etc/environment
         echo VUFIND_HOME="$VUFIND_HOME"  >> /etc/environment
         echo VUFIND_LOCAL_DIR="$VUFIND_LOCAL_DIR" >> /etc/environment
-        if [[ "${STACK_NAME}" != devel-* ]]; then
+        if [[ "${STACK_NAME}" == devel-* ]]; then
+            unset VUFIND_CACHE_DIR
+        else
             echo VUFIND_CACHE_DIR="$VUFIND_CACHE_DIR" >> /etc/environment
         fi
         echo VUFIND_LOCAL_MODULES="Catalog" >> /etc/environment
