fedff950481619d5ee70a0f9b6bbd693170af225
Nathan Collins <colli372@msu.edu>
2024-04-11 18:20:48 -0400

    PC-984 Better removal of session data
    Now cleans out sessions data 3 times a day and does so
    in larger batches, including removing session data
    just prior to creating a database backup. Move database
    backup creation back slightly to accomodate.

--- vufind/scripts/cron-sessions.sh
+++ vufind/scripts/cron-sessions.sh
@@ -2,7 +2,8 @@
 
 # Cron job - Clear out old sessions from the database
 
-export CRON_COMMAND="/usr/bin/php /usr/local/vufind/public/index.php util/expire_sessions"
+EXPIRE_IF_OLDER_THAN_AGE_IN_DAYS="0.5"
+export CRON_COMMAND="/usr/bin/php /usr/local/vufind/public/index.php util/expire_sessions --batch 10000 $EXPIRE_IF_OLDER_THAN_AGE_IN_DAYS"
 export LATEST_PATH=/mnt/logs/vufind/sessions_cleanup_latest.log
 export LOG_PATH=/mnt/logs/vufind/sessions_cleanup.log
 export EXIT_CODE_PATH=/mnt/logs/vufind/sessions_exit_code
