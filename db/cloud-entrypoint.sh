5c8139027caaee76dd03ff992b52e914d608dc12
Robby <roudonro@msu.edu>
2024-05-14 14:28:29 -0400

    [PC-758] Avoid using env variables for passwords (outside VuFind)

--- db/cloud-entrypoint.sh
+++ db/cloud-entrypoint.sh
@@ -7,9 +7,46 @@ set -o nounset
 set -o pipefail
 # set -o xtrace # Uncomment this line for debugging purposes
 
-# Set monitoring db password in sql file, then unset the env variable
+secret_env_variable_creation() {
+    # Array to store temporary environment variable names
+    declare -ga tmp_env_variables
+
+    # Loop through all environment variables
+    for var in $(compgen -e); do
+        # Check if variable name ends with _FILE
+        if [[ ! $var == *_FILE ]]; then
+            continue
+        fi
+
+        # Get the path stored in the variable
+        file_path="${!var}"
+        # Check if file exists and is readable
+        if [ ! -r "$file_path" ]; then
+            echo "Error: File $file_path for variable $var does not exist or is not readable." >&2
+            continue
+        fi
+
+        # Extract variable name without _FILE suffix
+        new_var_name="${var%_FILE}"
+        # Create new environment variable
+        export "$new_var_name"="$(<"$file_path")"
+        # Add variable name to tmp_env_variables array
+        tmp_env_variables+=("$new_var_name")
+    done
+}
+
+secret_env_variable_deletion() {
+    # Unset all variables stored in tmp_env_variables array
+    for var_name in "${tmp_env_variables[@]}"; do
+        unset "$var_name"
+    done
+    unset tmp_env_variables
+}
+
+secret_env_variable_creation
+# Set monitoring db password in sql file
 envsubst < /docker-entrypoint-initdb.d/monitoring.sql | sponge /docker-entrypoint-initdb.d/monitoring.sql
-unset MARIADB_MONITORING_PASSWORD
+secret_env_variable_deletion
 
 # Load libraries
 . /opt/bitnami/scripts/libbitnami.sh
