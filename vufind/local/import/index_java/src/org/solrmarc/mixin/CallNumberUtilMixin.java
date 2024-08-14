c4265636fccf2a3b817077dec8125f0525dc7ca6
Damien Guillaume <damieng@msu.edu>
2024-08-14 14:40:31 -0400

    [PC-1076] Custom method to extract call number label

---
+++ vufind/local/import/index_java/src/org/solrmarc/mixin/CallNumberUtilMixin.java
@@ -0,0 +1,48 @@
+package org.solrmarc.mixin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.Record;
+import org.marc4j.marc.VariableField;
+
+import org.solrmarc.index.SolrIndexerMixin;
+
+public class CallNumberUtilMixin extends SolrIndexerMixin {
+
+    /**
+     * We want this, but it doesn't work with Solrmarc (it returns fields in document order):
+     * callnumber-label = custom, getCallNumberLabel("952e:050a,first")
+     * Instead this method is called like this:
+     * callnumber-label = custom, getMSUCallNumberLabel
+     */
+    public String getMSUCallNumberLabel(final Record record) {
+        List<String> callNumbers = getValuesMatching(record, "952", "e");
+        if (callNumbers.isEmpty()) {
+            callNumbers = getValuesMatching(record, "050", "a");
+        }
+        if (callNumbers.isEmpty()) {
+            return null;
+        }
+        String res = callNumbers.get(0);
+        int dotPos = res.indexOf(".");
+        if (dotPos > 0) {
+            res = res.substring(0, dotPos);
+        }
+        return res.toUpperCase();
+    }
+
+    private List<String> getValuesMatching(Record record, String fieldCode, String subfieldCodes) {
+        List<VariableField> fields = record.getVariableFields(fieldCode);
+        List<String> values = new ArrayList<String>();
+        for (VariableField vf : fields) {
+            if (!(vf instanceof DataField)) {
+                continue;
+            }
+            DataField df = (DataField)vf;
+            values.addAll(df.getSubfields(subfieldCodes).stream().map(f -> f.getData()).toList());
+        }
+        return(values);
+    }
+}
