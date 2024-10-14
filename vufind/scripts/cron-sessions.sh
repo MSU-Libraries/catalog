502bf6b7ca09ad3d716b303fa062327c9687dfbc
Megan Schanz <schanzme@msu.edu>
2024-10-14 16:25:47 -0400

    PC-1148 fix permissions and a typo in session script

--- vufind/scripts/cron-sessions.sh
+++ vufind/scripts/cron-sessions.sh
@@ -2,7 +2,7 @@
 
 # Cron job - Clear out old sessions from the database
 
-export CRON_COMMAND="/usr/local/bin/pc-clear-sessions -v --batch-size 1000 --expiration-days 0.5 --max-attempt 5"
+export CRON_COMMAND="/usr/local/bin/pc-clear-sessions -v --batch-size 1000 --expiration-days 0.5 --max-attempts 5"
 export LATEST_PATH=/mnt/logs/vufind/sessions_cleanup_latest.log
 export LOG_PATH=/mnt/logs/vufind/sessions_cleanup.log
 export EXIT_CODE_PATH=/mnt/logs/vufind/sessions_exit_code
