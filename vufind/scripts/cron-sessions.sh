7d2cdf82851ca85b418af4fb18daab8616142c83
Megan Schanz <schanzme@msu.edu>
2024-10-14 10:28:42 -0400

    PC-1148 Shellcheck fix

--- vufind/scripts/cron-sessions.sh
+++ vufind/scripts/cron-sessions.sh
@@ -2,7 +2,6 @@
 
 # Cron job - Clear out old sessions from the database
 
-EXPIRE_IF_OLDER_THAN_AGE_IN_DAYS="0.5"
 export CRON_COMMAND="/usr/local/bin/pc-clear-sessions -v --batch-size 1000 --expiration-days 0.5 --max-attempt 5"
 export LATEST_PATH=/mnt/logs/vufind/sessions_cleanup_latest.log
 export LOG_PATH=/mnt/logs/vufind/sessions_cleanup.log
