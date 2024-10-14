2497a1d5462691af6ee0249814fd2a28b517778f
Megan Schanz <schanzme@msu.edu>
2024-10-14 10:23:30 -0400

    PC-1148 Create wrapper for clearing vufind sessions

--- vufind/scripts/cron-sessions.sh
+++ vufind/scripts/cron-sessions.sh
@@ -3,7 +3,7 @@
 # Cron job - Clear out old sessions from the database
 
 EXPIRE_IF_OLDER_THAN_AGE_IN_DAYS="0.5"
-export CRON_COMMAND="/usr/bin/php /usr/local/vufind/public/index.php util/expire_sessions --batch 1000 $EXPIRE_IF_OLDER_THAN_AGE_IN_DAYS"
+export CRON_COMMAND="/usr/local/bin/pc-clear-sessions -v --batch-size 1000 --expiration-days 0.5 --max-attempt 5"
 export LATEST_PATH=/mnt/logs/vufind/sessions_cleanup_latest.log
 export LOG_PATH=/mnt/logs/vufind/sessions_cleanup.log
 export EXIT_CODE_PATH=/mnt/logs/vufind/sessions_exit_code
