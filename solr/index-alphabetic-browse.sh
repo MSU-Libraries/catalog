469ca42b86a241638bdcca84647c59c5bc17913a
Damien Guillaume <damieng@msu.edu>
2024-11-19 16:18:02 -0500

    [PC-1222] Limit sort memory for alphabrowse

--- solr/index-alphabetic-browse.sh
+++ solr/index-alphabetic-browse.sh
@@ -76,6 +76,13 @@ index_dir="${SOLR_HOME}/alphabetical_browse"
 
 mkdir -p "$index_dir"
 
+function verbose
+{
+    LOG_TS=$(date +%Y-%m-%d\ %H:%M:%S)
+    MSG="[${LOG_TS}] $1"
+    echo "${MSG}"
+}
+
 function build_browse
 {
     browse=$1
@@ -84,7 +91,9 @@ function build_browse
 
     extra_jvm_opts=$4
 
-    # Get the browse headings
+    verbose "Build browse: ${browse}"
+
+    verbose "  Get the browse headings"
     if [ "$skip_authority" = "1" ]; then
         # shellcheck disable=SC2086
         # extra_jvm_opts may have several options (not currently used), in which case we want it to expand
@@ -101,36 +110,37 @@ function build_browse
         fi
     fi
 
-    # Sort the browse headings
-    if ! output=$(sort -T /var/tmp -u -t$'\1' -k1 "${browse}.tmp" -o "sorted-${browse}.tmp" 2>&1); then
+    verbose "  Sort the browse headings"
+    if ! output=$(sort -T /var/tmp --buffer-size=1G -u -t$'\1' -k1 "${browse}.tmp" -o "sorted-${browse}.tmp" 2>&1); then
         echo "ERROR: Failed to sort ${browse}. ${output}."
         exit 1
     fi
 
-    # Build the SQLite database
+    verbose "  Build the SQLite database"
     if ! output=$($JAVA -Dfile.encoding="UTF-8" -cp "$CLASSPATH" CreateBrowseSQLite "sorted-${browse}.tmp" "${browse}_browse.db" 2>&1); then
         echo "ERROR: Failed to build the SQLite database for ${browse}. ${output}."
         exit 1
     fi
 
-    # Clear up temp files
+    verbose "  Clear up temp files"
     if ! output=$(rm -f -- *.tmp 2>&1); then
         echo "ERROR: Failed to clear out temp files for ${browse}. ${output}."
         exit 1
     fi
 
-    # Move the new database to the index directory
+    verbose "  Move the new database to the index directory"
     if ! output=$(mv "${browse}_browse.db" "$index_dir/${browse}_browse.db-updated" 2>&1); then
         echo "ERROR: Failed to move ${browse}_browse.db database to ${index_dir}/${browse}_browse.db-updated. ${output}."
         exit 1
     fi
 
-    # Indicate that the new database is ready for use
+    verbose "  Indicate that the new database is ready for use"
     if ! output=$(touch "$index_dir/${browse}_browse.db-ready" 2>&1); then
         echo "ERROR: Failed to mark the new ${browse} database as ready for use. ${output}."
         exit 1
     fi
 }
+
 # These parameters should match the ones in solr/vufind/biblio/conf/solrconfig.xml - BrowseRequestHandler
 build_browse "hierarchy" "hierarchy_browse"
 build_browse "title" "title_browse" 0 "-Dbibleech=StoredFieldLeech -Dsortfield=title_browse_sort -Dvaluefield=title_browse -Dbrowse.normalizer=org.vufind.util.TitleNormalizer"
